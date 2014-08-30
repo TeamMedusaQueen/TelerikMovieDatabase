3c86e30a830a9ecedae35a946bc97e97a3dc53ba
sophialazarova <sophia.lazarova@yahoo.com>
2014-08-30 15:19:30 +0300

    movies - people connection fix

--- Application/TMDB/TMDB.Models/Person.cs
+++ Application/TMDB/TMDB.Models/Person.cs
@@ -8,10 +8,12 @@
     public class Person : BaseEntity
     {
         private ICollection<JobPosition> job;
+        private ICollection<Movie> movies;
 
         public Person()
         {
             this.job = new HashSet<JobPosition>();
+            this.movies = new HashSet<Movie>();
         }
 
         public ICollection<JobPosition> Job
@@ -23,6 +25,18 @@
             set
             {
                 this.job = value;
+            }
+        }
+
+        public ICollection<Movie> Movies
+        {
+            get
+            {
+                return this.movies;
+            }
+            set
+            {
+                this.movies = value;
             }
         }
 
