e889332f479a297d755a2eebbc3d1eb7b3347f06
sophialazarova <sophia.lazarova@yahoo.com>
2014-08-30 12:20:06 +0300

    class Person

--- Application/IMDb/IMDB.Models/Movie.cs
+++ Application/IMDb/IMDB.Models/Movie.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IMDB.Models
+﻿namespace IMDB.Models
 {
-    class Movie
+    using System;
+    using System.Linq;
+
+    public class Movie
     {
+        public int MovieID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Storyline { get; set; }
+
+        public int RunningTime { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int DirectorID { get; set; }
+
+        public int GenreID { get; set; }
+
+        public int BoxOfficeEntry { get; set; }
+
+        public decimal Gross { get; set; }
     }
 }
