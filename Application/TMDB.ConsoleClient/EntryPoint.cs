ff635cf2d4f15812b5ef2ca5c0f40907f0a1ff30
sophialazarova <sophia.lazarova@yahoo.com>
2014-08-31 21:44:13 +0300

    Writting data in excel file

--- Application/TMDB.ConsoleClient/EntryPoint.cs
+++ Application/TMDB.ConsoleClient/EntryPoint.cs
@@ -1,120 +1,117 @@
 ﻿namespace TMDB.ConsoleClient
 {
-	using System;
-	using System.Collections.Generic;
-	using System.IO;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
-	using System.Xml;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
     using System.Data.SQLite;
+    using System.Data.OleDb;
 
-	internal class Program
-	{
-		private static void Main()
-		{
-            var result  = ConnectToSqLiteDatabase();
-            foreach (var entry in result)
-            {
-                Console.WriteLine(entry);
-            }
-			////ImportMovieAwardsAndNominationsFromXML();
+    internal class Program
+    {
+        private static void Main()
+        {
+
+            ////ImportMovieAwardsAndNominationsFromXML();
             //
-			//using (var dbContext = new TMDB.Data.TmdbContext())
-			//{
-			//	var movies = dbContext.Movies.ToArray();
+            //using (var dbContext = new TMDB.Data.TmdbContext())
+            //{
+            //	var movies = dbContext.Movies.ToArray();
             //  
             //
-			//	//var movieJSONModels = OMDB.GetTop250();
-			//	//foreach (var movieJSONModel in movieJSONModels)
-			//	//{
-			//	//	var movie = movieJSONModel.GetMovieModel(dbContext);
-			//	//	dbContext.Movies.Add(movie);
-			//	//}
+            //	//var movieJSONModels = OMDB.GetTop250();
+            //	//foreach (var movieJSONModel in movieJSONModels)
+            //	//{
+            //	//	var movie = movieJSONModel.GetMovieModel(dbContext);
+            //	//	dbContext.Movies.Add(movie);
+            //	//}
             //
-			//	//dbContext.SaveChanges();
-			//}
+            //	//dbContext.SaveChanges();
+            //}
             //
-			////var mongoDbContext = new TMDB.Data.Provider.MongoDatabase.TmdbMongoDbContext();
-			////mongoDbContext.InitialCreate();
-		}
+            ////var mongoDbContext = new TMDB.Data.Provider.MongoDatabase.TmdbMongoDbContext();
+            ////mongoDbContext.InitialCreate();
+        }
 
-		private static void ImportMovieAwardsAndNominationsFromXML()
-		{
-			const string movieAward = "movie-awards";
-			const string movieNomination = "movie-nominations";
-			const string industryAwardsNode = "industry-awards";
+        private static void ImportMovieAwardsAndNominationsFromXML()
+        {
+            const string movieAward = "movie-awards";
+            const string movieNomination = "movie-nominations";
+            const string industryAwardsNode = "industry-awards";
 
-			var result = new StringBuilder();
+            var result = new StringBuilder();
 
-			var nominations = new List<string>();
-			var awards = new List<string>();
-			List<string> currentCollection = null;
+            var nominations = new List<string>();
+            var awards = new List<string>();
+            List<string> currentCollection = null;
 
-			// Create an XmlReader
-			using (XmlReader reader = XmlReader.Create(new StringReader(File.ReadAllText("Data.xml"))))
-			{
-				XmlWriterSettings ws = new XmlWriterSettings();
-				ws.Indent = true;
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create(new StringReader(File.ReadAllText("Data.xml"))))
+            {
+                XmlWriterSettings ws = new XmlWriterSettings();
+                ws.Indent = true;
 
-				bool isInAwardNode = false;
-				bool isInNominationNode = false;
+                bool isInAwardNode = false;
+                bool isInNominationNode = false;
 
-				// Parse the file and display each of the nodes.
-				while (reader.Read())
-				{
-					switch (reader.NodeType)
-					{
-						case XmlNodeType.Element:
-							if (!(isInAwardNode || isInNominationNode))
-							{
-								isInAwardNode = reader.Name == movieAward;
-								isInNominationNode = reader.Name == movieNomination;
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (!(isInAwardNode || isInNominationNode))
+                            {
+                                isInAwardNode = reader.Name == movieAward;
+                                isInNominationNode = reader.Name == movieNomination;
 
-								if (isInAwardNode)
-								{
-									currentCollection = awards;
-								}
+                                if (isInAwardNode)
+                                {
+                                    currentCollection = awards;
+                                }
 
-								if (isInNominationNode)
-								{
-									currentCollection = nominations;
-								}
-							}
-							else if (reader.Name == industryAwardsNode)
-							{
-								var awardYear = reader.GetAttribute("year");
-								var awardName = reader.ReadElementContentAsString();
-								currentCollection.Add("Name: " + awardName + " Year: " + awardYear);
-							}
-							break;
+                                if (isInNominationNode)
+                                {
+                                    currentCollection = nominations;
+                                }
+                            }
+                            else if (reader.Name == industryAwardsNode)
+                            {
+                                var awardYear = reader.GetAttribute("year");
+                                var awardName = reader.ReadElementContentAsString();
+                                currentCollection.Add("Name: " + awardName + " Year: " + awardYear);
+                            }
+                            break;
 
-						case XmlNodeType.EndElement:
-							if (reader.Name != industryAwardsNode)
-							{
-								isInAwardNode = false;
-								isInNominationNode = false;
-								currentCollection = null;
-							}
-							break;
-					}
-				}
-			}
+                        case XmlNodeType.EndElement:
+                            if (reader.Name != industryAwardsNode)
+                            {
+                                isInAwardNode = false;
+                                isInNominationNode = false;
+                                currentCollection = null;
+                            }
+                            break;
+                    }
+                }
+            }
 
-			Console.WriteLine("Nominations:");
-			foreach (var nomination in nominations)
-			{
-				Console.WriteLine(nomination);
-			}
+            Console.WriteLine("Nominations:");
+            foreach (var nomination in nominations)
+            {
+                Console.WriteLine(nomination);
+            }
 
-			Console.WriteLine("Awards:");
-			foreach (var award in awards)
-			{
-				Console.WriteLine(award);
-			}
-		}
+            Console.WriteLine("Awards:");
+            foreach (var award in awards)
+            {
+                Console.WriteLine(award);
+            }
+        }
 
-        private static List<Object> ConnectToSqLiteDatabase()
+        private static void ManageSQLiteDataBaseExcelConnection()
         {
             SQLiteConnection sqliteConnection = new SQLiteConnection("Data Source = ..\\..\\..\\..\\Databases\\SQLite\\MovieBudget.db");
             sqliteConnection.Open();
@@ -131,10 +128,27 @@
                     data.Add(reportId);
                     data.Add(title);
                     data.Add(budget);
-                    //InsertInExcel("Bugs", carModel, description, importance);
+                    InsertInExcel(reportId, title, budget);
                 }
             }
-            return data;
+            sqliteConnection.Close();
+            Console.WriteLine("Data inserted in Excel file successfully.");
         }
-	}
+
+        private static void InsertInExcel(int reportId, string title, int budget)
+        {
+            OleDbConnection excelConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\..\\..\\..\\Databases\\SQLite\\MovieBudgetReportList.xlsx;Extended Properties=Excel 12.0;");
+            excelConnection.Open();
+
+            using (excelConnection)
+            {
+                OleDbCommand insertCommand = new OleDbCommand("INSERT INTO [Sheet1$] (ReportID, Title, Budget) VALUES (@reportId, @title, @budget)", excelConnection);
+                insertCommand.Parameters.AddWithValue("@ReportID", reportId);
+                insertCommand.Parameters.AddWithValue("@Title", title);
+                insertCommand.Parameters.AddWithValue("@Budget", budget);
+
+                insertCommand.ExecuteNonQuery();
+            }
+        }
+    }
 }
