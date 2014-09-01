0c93026808d4a869ac6ca0731b79ed1085079701
ventsislav-georgiev <ventsislav.georgiev@live.com>
2014-09-01 22:12:23 +0300

    Namespace fixes

--- Application/TelerikMovieDatabase.ConsoleClient/EntryPoint.cs
+++ Application/TelerikMovieDatabase.ConsoleClient/EntryPoint.cs
@@ -1,4 +1,4 @@
-﻿namespace TMDB.ConsoleClient
+﻿namespace TelerikMovieDatabase.ConsoleClient
 {
     using System;
     using System.Collections.Generic;
--- Application/TelerikMovieDatabase.Data.MySql/Properties/AssemblyInfo.cs
+++ Application/TelerikMovieDatabase.Data.MySql/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TelerikMovieDatabase.DatabaseDataGet")]
+[assembly: AssemblyTitle("TelerikMovieDatabase.Data.MySql")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TelerikMovieDatabase.DatabaseDataGet")]
+[assembly: AssemblyProduct("TelerikMovieDatabase.Data.MySql")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
