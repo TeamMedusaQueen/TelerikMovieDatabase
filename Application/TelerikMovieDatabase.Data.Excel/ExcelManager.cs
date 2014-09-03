fa823f4ffdb9264e3631d5202a773642789c13e4
andrei-pl <andrey.pld@gmail.com>
2014-09-03 14:35:27 +0300

    Import in SQL from XLS added Added again after last github crash

--- Application/TelerikMovieDatabase.Data.Excel/ExcelManager.cs
+++ Application/TelerikMovieDatabase.Data.Excel/ExcelManager.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.OleDb;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TelerikMovieDatabase.Data.Excel
+﻿namespace TelerikMovieDatabase.Data.Excel
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data.OleDb;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using TelerikMovieDatabase.Data.MsSql;
+    using TelerikMovieDatabase.Models;
+
     public class ExcelManager
     {
         public static void InsertInExcelFile(int reportId, string title, int profits)
@@ -22,6 +26,67 @@ namespace TelerikMovieDatabase.Data.Excel
                 insertCommand.Parameters.AddWithValue("@Profits", profits);
 
                 insertCommand.ExecuteNonQuery();
+            }
+        }
+
+        public static void ImportInSqlDb(TelerikMovieDatabaseMsSqlContext db)
+        {
+            const string ReportsPath = "..\\..\\..\\..\\Databases\\XLS\\XLSData";
+            const string directoryPathForArchive = "..\\..\\..\\..\\Databases\\XLS\\Reports";
+            const string zipFilePath = "..\\..\\..\\..\\Databases\\XLS\\";
+            const string zipFileName = "ReportsArchive.zip";
+
+            if (!Directory.Exists(ReportsPath))
+            {
+                Directory.CreateDirectory(ReportsPath);
+            }
+
+            List<string> files = new List<string>();
+            var directories = Directory.GetDirectories(ReportsPath);
+
+            foreach (var folder in directories)
+            {
+                files.AddRange(Directory.GetFiles(folder));
+            }
+
+            foreach (var file in files)
+            {
+                OleDbConnection xlsConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + file + ";Extended Properties=\"Excel 12.0 Xml;HDR=YES\";");
+
+                xlsConn.Open();
+
+                using (db)
+                {
+                    using (xlsConn)
+                    {
+                        OleDbCommand command = new OleDbCommand("SELECT * FROM [Sheet1$]", xlsConn);
+                        OleDbDataReader reader = command.ExecuteReader();
+
+                        using (reader)
+                        {
+                            while (reader.Read())
+                            {
+                                double weeks = (double)reader["Weeks"];
+                                string score = (string)reader["Budget"];
+                                Console.WriteLine("{0} - {1}", weeks, score);
+
+                                db.BoxOfficeEntries.Add(
+                                    new BoxOfficeEntry
+                                    {
+                                        Weeks = (int)weeks,
+                                        GeneratedWeekendIncome = decimal.Parse(score)
+                                    });
+
+                                db.SaveChanges();
+                            }
+
+                            foreach (var item in db.BoxOfficeEntries)
+                            {
+                                Console.WriteLine(item.ID + " " + item.Weeks + " - > " + item.GeneratedWeekendIncome);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
