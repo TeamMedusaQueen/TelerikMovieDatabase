b5af224174add055cf6b68caafcc5c0fe3da59e4
andrei-pl <andrey.pld@gmail.com>
2014-09-03 19:30:53 +0300

    UI first steps

---
+++ Application/TelerikMocieDatabase.UIClient/TelerikMovieDatabaseUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelerikMovieDatabase.UIClient
+{
+    public partial class frmTMDB : Form
+    {
+        public frmTMDB()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
