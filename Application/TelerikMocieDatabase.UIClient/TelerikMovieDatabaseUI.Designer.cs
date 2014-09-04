fefc87200d8bafd044bc71ea90d841a677928df3
andrei-pl <andrey.pld@gmail.com>
2014-09-04 04:41:24 +0300

    UI All Movies Info

--- Application/TelerikMocieDatabase.UIClient/TelerikMovieDatabaseUI.Designer.cs
+++ Application/TelerikMocieDatabase.UIClient/TelerikMovieDatabaseUI.Designer.cs
@@ -28,23 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTMDB));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.cmbImport = new System.Windows.Forms.ComboBox();
             this.cmbExport = new System.Windows.Forms.ComboBox();
             this.btnImport = new System.Windows.Forms.Button();
             this.cmbExportInfo = new System.Windows.Forms.ComboBox();
             this.btnOpenReport = new System.Windows.Forms.Button();
-            this.listInfo = new System.Windows.Forms.ListBox();
             this.btnMovies = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.groupCategories = new System.Windows.Forms.GroupBox();
-            this.chkGenre = new System.Windows.Forms.CheckBox();
-            this.chkDirector = new System.Windows.Forms.CheckBox();
-            this.chkActors = new System.Windows.Forms.CheckBox();
-            this.chkWriters = new System.Windows.Forms.CheckBox();
-            this.chkAwards = new System.Windows.Forms.CheckBox();
             this.chkAllInfo = new System.Windows.Forms.CheckBox();
+            this.chkAwards = new System.Windows.Forms.CheckBox();
+            this.chkWriters = new System.Windows.Forms.CheckBox();
+            this.chkActors = new System.Windows.Forms.CheckBox();
+            this.chkDirector = new System.Windows.Forms.CheckBox();
+            this.chkGenre = new System.Windows.Forms.CheckBox();
+            this.listInfo = new System.Windows.Forms.ListBox();
             this.groupCategories.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,7 +53,7 @@
             // 
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(989, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1230, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -119,15 +120,6 @@
             this.btnOpenReport.Text = "Open report";
             this.btnOpenReport.UseVisualStyleBackColor = true;
             // 
-            // listInfo
-            // 
-            this.listInfo.FormattingEnabled = true;
-            this.listInfo.Location = new System.Drawing.Point(153, 24);
-            this.listInfo.Name = "listInfo";
-            this.listInfo.Size = new System.Drawing.Size(824, 485);
-            this.listInfo.TabIndex = 6;
-            this.listInfo.TabStop = false;
-            // 
             // btnMovies
             // 
             this.btnMovies.Location = new System.Drawing.Point(12, 24);
@@ -136,6 +128,7 @@
             this.btnMovies.TabIndex = 7;
             this.btnMovies.Text = "All movies";
             this.btnMovies.UseVisualStyleBackColor = true;
+            this.btnMovies.Click += new System.EventHandler(this.btnMovies_Click);
             // 
             // btnSearch
             // 
@@ -170,45 +163,15 @@
             this.groupCategories.TabStop = false;
             this.groupCategories.Text = "Categories";
             // 
-            // chkGenre
+            // chkAllInfo
             // 
-            this.chkGenre.AutoSize = true;
-            this.chkGenre.Location = new System.Drawing.Point(7, 20);
-            this.chkGenre.Name = "chkGenre";
-            this.chkGenre.Size = new System.Drawing.Size(55, 17);
-            this.chkGenre.TabIndex = 0;
-            this.chkGenre.Text = "Genre";
-            this.chkGenre.UseVisualStyleBackColor = true;
-            // 
-            // chkDirector
-            // 
-            this.chkDirector.AutoSize = true;
-            this.chkDirector.Location = new System.Drawing.Point(7, 43);
-            this.chkDirector.Name = "chkDirector";
-            this.chkDirector.Size = new System.Drawing.Size(63, 17);
-            this.chkDirector.TabIndex = 11;
-            this.chkDirector.Text = "Director";
-            this.chkDirector.UseVisualStyleBackColor = true;
-            // 
-            // chkActors
-            // 
-            this.chkActors.AutoSize = true;
-            this.chkActors.Location = new System.Drawing.Point(7, 66);
-            this.chkActors.Name = "chkActors";
-            this.chkActors.Size = new System.Drawing.Size(56, 17);
-            this.chkActors.TabIndex = 12;
-            this.chkActors.Text = "Actors";
-            this.chkActors.UseVisualStyleBackColor = true;
-            // 
-            // chkWriters
-            // 
-            this.chkWriters.AutoSize = true;
-            this.chkWriters.Location = new System.Drawing.Point(6, 89);
-            this.chkWriters.Name = "chkWriters";
-            this.chkWriters.Size = new System.Drawing.Size(59, 17);
-            this.chkWriters.TabIndex = 13;
-            this.chkWriters.Text = "Writers";
-            this.chkWriters.UseVisualStyleBackColor = true;
+            this.chkAllInfo.AutoSize = true;
+            this.chkAllInfo.Location = new System.Drawing.Point(6, 136);
+            this.chkAllInfo.Name = "chkAllInfo";
+            this.chkAllInfo.Size = new System.Drawing.Size(92, 17);
+            this.chkAllInfo.TabIndex = 15;
+            this.chkAllInfo.Text = "All Information";
+            this.chkAllInfo.UseVisualStyleBackColor = true;
             // 
             // chkAwards
             // 
@@ -220,32 +183,72 @@
             this.chkAwards.Text = "Awards";
             this.chkAwards.UseVisualStyleBackColor = true;
             // 
-            // chkAllInfo
+            // chkWriters
             // 
-            this.chkAllInfo.AutoSize = true;
-            this.chkAllInfo.Location = new System.Drawing.Point(6, 136);
-            this.chkAllInfo.Name = "chkAllInfo";
-            this.chkAllInfo.Size = new System.Drawing.Size(92, 17);
-            this.chkAllInfo.TabIndex = 15;
-            this.chkAllInfo.Text = "All Information";
-            this.chkAllInfo.UseVisualStyleBackColor = true;
+            this.chkWriters.AutoSize = true;
+            this.chkWriters.Location = new System.Drawing.Point(6, 89);
+            this.chkWriters.Name = "chkWriters";
+            this.chkWriters.Size = new System.Drawing.Size(59, 17);
+            this.chkWriters.TabIndex = 13;
+            this.chkWriters.Text = "Writers";
+            this.chkWriters.UseVisualStyleBackColor = true;
+            // 
+            // chkActors
+            // 
+            this.chkActors.AutoSize = true;
+            this.chkActors.Location = new System.Drawing.Point(7, 66);
+            this.chkActors.Name = "chkActors";
+            this.chkActors.Size = new System.Drawing.Size(56, 17);
+            this.chkActors.TabIndex = 12;
+            this.chkActors.Text = "Actors";
+            this.chkActors.UseVisualStyleBackColor = true;
+            // 
+            // chkDirector
+            // 
+            this.chkDirector.AutoSize = true;
+            this.chkDirector.Location = new System.Drawing.Point(7, 43);
+            this.chkDirector.Name = "chkDirector";
+            this.chkDirector.Size = new System.Drawing.Size(63, 17);
+            this.chkDirector.TabIndex = 11;
+            this.chkDirector.Text = "Director";
+            this.chkDirector.UseVisualStyleBackColor = true;
+            // 
+            // chkGenre
+            // 
+            this.chkGenre.AutoSize = true;
+            this.chkGenre.Location = new System.Drawing.Point(7, 20);
+            this.chkGenre.Name = "chkGenre";
+            this.chkGenre.Size = new System.Drawing.Size(55, 17);
+            this.chkGenre.TabIndex = 0;
+            this.chkGenre.Text = "Genre";
+            this.chkGenre.UseVisualStyleBackColor = true;
+            // 
+            // listInfo
+            // 
+            this.listInfo.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listInfo.FormattingEnabled = true;
+            this.listInfo.Location = new System.Drawing.Point(147, 24);
+            this.listInfo.Name = "listInfo";
+            this.listInfo.Size = new System.Drawing.Size(1071, 485);
+            this.listInfo.TabIndex = 11;
             // 
             // frmTMDB
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 529);
+            this.ClientSize = new System.Drawing.Size(1230, 529);
+            this.Controls.Add(this.listInfo);
             this.Controls.Add(this.groupCategories);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnMovies);
-            this.Controls.Add(this.listInfo);
             this.Controls.Add(this.btnOpenReport);
             this.Controls.Add(this.cmbExportInfo);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.cmbExport);
             this.Controls.Add(this.cmbImport);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmTMDB";
             this.Text = "Telerik Movie Database";
@@ -265,7 +268,6 @@
         private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.ComboBox cmbExportInfo;
         private System.Windows.Forms.Button btnOpenReport;
-        private System.Windows.Forms.ListBox listInfo;
         private System.Windows.Forms.Button btnMovies;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtSearch;
@@ -276,6 +278,7 @@
         private System.Windows.Forms.CheckBox chkActors;
         private System.Windows.Forms.CheckBox chkDirector;
         private System.Windows.Forms.CheckBox chkGenre;
+        private System.Windows.Forms.ListBox listInfo;
     }
 }
 
--- Application/TelerikMovieDatabase.ConsoleClient/EntryPoint.cs
+++ Application/TelerikMovieDatabase.ConsoleClient/EntryPoint.cs
@@ -15,96 +15,96 @@
 
 	internal class EntryPoint
 	{
-		// Tasks:
-		// C#
-		// Prelimirary data
-		// ZIP File Excel 2003 => SQL Server
-		// MongoDB => SQL Server
-		//
-		// XML file => SQL Server and MongoDB
-		// SQLite + MySQL => Excel 2007 (.xlsx)
-		// SQL Server => JSON => MySQL
-		//
-		// Reports
-		// SQL Server => XML
-		// SQL Server => PDF
-		private static void Main()
-		{
-			// Prepare Initial Data
-			// Step 1
-			InitializeMongoDb();
-			// Step 2
-			//Zip xls file to archive, extracting and import to sqlDB
-			using (var dbContext = new TelerikMovieDatabaseMsSqlContext())
-			{
-				ZipManager.AddFileToZipArchive("..\\..\\..\\..\\Databases\\XLS\\XLSData\\BoxOffice-week1-September.xls", File.GetCreationTime("..\\..\\..\\..\\Databases\\XLS\\XLSData\\BoxOffice-week1-September.xls").ToString("dd-MMM-yyyy"), "..\\..\\..\\..\\Databases\\XLS\\Reports.zip");
-				ZipManager.ExtractFiles("..\\..\\..\\..\\Databases\\XLS\\Reports.zip", "..\\..\\..\\..\\Databases\\XLS\\Reports\\");
-				ExcelManager.ImportInSqlDb(dbContext, "..\\..\\..\\..\\Databases\\XLS\\Reports\\"); //If you want to add in database first uncoment the code inside this method
-			}
-			// Step 3
-			//Create SqLite Database and fill data ?
-			// Step 4
-			//Create MySql Database and fill data ?
+        // Tasks:
+        // C#
+        // Prelimirary data
+        // ZIP File Excel 2003 => SQL Server
+        // MongoDB => SQL Server
+        //
+        // XML file => SQL Server and MongoDB
+        // SQLite + MySQL => Excel 2007 (.xlsx)
+        // SQL Server => JSON => MySQL
+        //
+        // Reports
+        // SQL Server => XML
+        // SQL Server => PDF
+        private static void Main()
+        {
+            // Prepare Initial Data
+            // Step 1
+            InitializeMongoDb();
+            // Step 2
+            //Zip xls file to archive, extracting and import to sqlDB
+            using (var dbContext = new TelerikMovieDatabaseMsSqlContext())
+            {
+                ZipManager.AddFileToZipArchive("..\\..\\..\\..\\Databases\\XLS\\XLSData\\BoxOffice-week1-September.xls", File.GetCreationTime("..\\..\\..\\..\\Databases\\XLS\\XLSData\\BoxOffice-week1-September.xls").ToString("dd-MMM-yyyy"), "..\\..\\..\\..\\Databases\\XLS\\Reports.zip");
+                ZipManager.ExtractFiles("..\\..\\..\\..\\Databases\\XLS\\Reports.zip", "..\\..\\..\\..\\Databases\\XLS\\Reports\\");
+                ExcelManager.ImportInSqlDb(dbContext, "..\\..\\..\\..\\Databases\\XLS\\Reports\\"); //If you want to add in database first uncoment the code inside this method
+            }
+            // Step 3
+            //Create SqLite Database and fill data ?
+            // Step 4
+            //Create MySql Database and fill data ?
 
-			// Step 5
-			// Migrate Data From MongoDb To MsSql
-			MigrateDataFromMongoDbToMsSql();
+            // Step 5
+            // Migrate Data From MongoDb To MsSql
+            MigrateDataFromMongoDbToMsSql();
 
-			// Export all data to xml
-			using (var data = new TelerikMovieDatabaseMsSqlData())
-			{
-				XmlReport.ExportToXml<IGenericRepository<Person>, Person>(data.Persons, "Persons.xml", p => p.Jobs);
-				XmlReport.ExportToXml<IGenericRepository<Language>, Language>(data.Languages, "Languages.xml");
-				XmlReport.ExportToXml<IGenericRepository<Country>, Country>(data.Countries, "Countries.xml");
-				XmlReport.ExportToXml<IGenericRepository<JobPosition>, JobPosition>(data.JobPositions, "JobPositions.xml");
-				XmlReport.ExportToXml<IGenericRepository<Genre>, Genre>(data.Genres, "Genres.xml");
+            // Export all data to xml
+            using (var data = new TelerikMovieDatabaseMsSqlData())
+            {
+                XmlReport.ExportToXml<IGenericRepository<Person>, Person>(data.Persons, "Persons.xml", p => p.Jobs);
+                XmlReport.ExportToXml<IGenericRepository<Language>, Language>(data.Languages, "Languages.xml");
+                XmlReport.ExportToXml<IGenericRepository<Country>, Country>(data.Countries, "Countries.xml");
+                XmlReport.ExportToXml<IGenericRepository<JobPosition>, JobPosition>(data.JobPositions, "JobPositions.xml");
+                XmlReport.ExportToXml<IGenericRepository<Genre>, Genre>(data.Genres, "Genres.xml");
 
-				// Custom report ( All movies after 2000 year exported with direcotr and actors )
-				XmlReport.ExportToXml<IGenericRepository<Movie>, Movie>(
-					data.Movies,
-					"MoviesAfter2000.xml",
-					movie => movie.ReleaseDate.HasValue && movie.ReleaseDate.Value.Year > 2000,
-					movie => movie.Director,
-					movie => movie.Cast);
-			}
+                // Custom report ( All movies after 2000 year exported with direcotr and actors )
+                XmlReport.ExportToXml<IGenericRepository<Movie>, Movie>(
+                    data.Movies,
+                    "MoviesAfter2000.xml",
+                    movie => movie.ReleaseDate.HasValue && movie.ReleaseDate.Value.Year > 2000,
+                    movie => movie.Director,
+                    movie => movie.Cast);
+            }
 
-			// Import all data from xml
-			var persons = XmlReport.ImportFromXml<Person[]>("Persons.xml");
-			var languages = XmlReport.ImportFromXml<Language[]>("Languages.xml");
-			var countries = XmlReport.ImportFromXml<Country[]>("Countries.xml");
-			var jobPositions = XmlReport.ImportFromXml<JobPosition[]>("JobPositions.xml");
-			var genres = XmlReport.ImportFromXml<Genre[]>("Genres.xml");
-			var moviesAfter2000 = XmlReport.ImportFromXml<Movie[]>("MoviesAfter2000.xml");
-		}
+            // Import all data from xml
+            var persons = XmlReport.ImportFromXml<Person[]>("Persons.xml");
+            var languages = XmlReport.ImportFromXml<Language[]>("Languages.xml");
+            var countries = XmlReport.ImportFromXml<Country[]>("Countries.xml");
+            var jobPositions = XmlReport.ImportFromXml<JobPosition[]>("JobPositions.xml");
+            var genres = XmlReport.ImportFromXml<Genre[]>("Genres.xml");
+            var moviesAfter2000 = XmlReport.ImportFromXml<Movie[]>("MoviesAfter2000.xml");
+        }
 
-		private static void InitializeMongoDb()
-		{
-			IEnumerable<Movie> movies;
-			using (var dbContext = new TelerikMovieDatabaseMsSqlContext())
-			{
-				movies = OpenMovieDatabase.GetTop250()
-					.Select(movieJson => movieJson.GetMovieModel(dbContext));
-			}
+        private static void InitializeMongoDb()
+        {
+            IEnumerable<Movie> movies;
+            using (var dbContext = new TelerikMovieDatabaseMsSqlContext())
+            {
+                movies = OpenMovieDatabase.GetTop250()
+                    .Select(movieJson => movieJson.GetMovieModel(dbContext));
+            }
 
-			new MongoDbInitializer().Init(movies);
-		}
+            new MongoDbInitializer().Init(movies);
+        }
 
-		private static void MigrateDataFromMongoDbToMsSql()
-		{
-			using (var dbContext = new TelerikMovieDatabaseMsSqlContext())
-			{
-				// TODO: This condition should be removed, when UI is ready the migration process will be invoked from button
-				if (!dbContext.Movies.Any())
-				{
-					var movies = new MongoDbToMsSqlMigration().GetMovies();
-					foreach (var movie in movies)
-					{
-						dbContext.Movies.Add(movie);
-					}
+        private static void MigrateDataFromMongoDbToMsSql()
+        {
+            using (var dbContext = new TelerikMovieDatabaseMsSqlContext())
+            {
+                // TODO: This condition should be removed, when UI is ready the migration process will be invoked from button
+                if (!dbContext.Movies.Any())
+                {
+                    var movies = new MongoDbToMsSqlMigration().GetMovies();
+                    foreach (var movie in movies)
+                    {
+                        dbContext.Movies.Add(movie);
+                    }
 
-					dbContext.SaveChanges();
-				}
-			}
-		}
+                    dbContext.SaveChanges();
+                }
+            }
+        }
 	}
 }
